f729a0910ca234f916ee81f93a9fc48e999c6271
Endnes <sergeev.krut@gmail.com>
2022-12-24 10:59:26 +0300

    first commit

---
+++ Lesson_03_Example_001/Program.cs
@@ -0,0 +1,61 @@
+﻿int GetNumberOfQuater(int x, int y)
+{
+    if (x == 0 || y == 0){
+        return -1;
+    }
+
+    if (x > 0 && y > 0){
+        return 1;
+    }
+    else if (x < 0 && y > 0){
+        return 2;
+    }
+    else if (x < 0 && y < 0){
+        return 3;
+    }
+    else if (x > 0 && y < 0){
+        return 4;
+    }
+    else {
+        return -1;
+    }
+}
+
+void PrintNumberOfQuater(int x, int y)
+{
+    if (x == 0 || y == 0){
+        Console.WriteLine($"Номер четверти не определен для точки ({x}, {y})");
+    }
+
+    if (x > 0 && y > 0){
+        Console.WriteLine($"Номер четверти координаты ({x}, {y}) равен {1}");
+    }
+    else if (x < 0 && y > 0){
+        Console.WriteLine($"Номер четверти координаты ({x}, {y}) равен {2}");
+    }
+    else if (x < 0 && y < 0){
+        Console.WriteLine($"Номер четверти координаты ({x}, {y}) равен {3}");
+    }
+    else if (x > 0 && y < 0){
+        Console.WriteLine($"Номер четверти координаты ({x}, {y}) равен {4}");
+    }
+    else {
+        Console.WriteLine($"Номер четверти не определен для точки ({x}, {y})");
+    }
+}
+
+int x = 5;
+int y = -10;
+
+int quater = GetNumberOfQuater(x, y);
+
+if (quater == -1)
+{
+    Console.WriteLine($"Номер четверти не определен для точки ({x}, {y})");
+} 
+else 
+{
+    Console.WriteLine($"Номер четверти координаты ({x}, {y}) равен {GetNumberOfQuater(x, y)}");
+}
+
+PrintNumberOfQuater(x, y);
---
+++ Lesson_03_Example_002/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапозон возможных координат точек в этой четверти (х и у).
+
+void PrintCoordinatRange (int number)
+{
+    if (number == 4)
+    {
+        Console.WriteLine("Диапозон координат: x = [0; infinity], y = [0; -infinity]");
+    }
+    else if (number == 3)
+    {
+        Console.WriteLine("Диапозон координат: x = [0; -infinity], y = [0; -infinity]");
+    }
+    else if (number == 2)
+    {
+        Console.WriteLine("Диапозон координат: x = [0; -infinity], y = [0; infinity]");
+    }
+    else if (number == 1)
+    {
+        Console.WriteLine("Диапозон координат: x = [0; infinity], y = [0; infinity]");
+    }
+    else 
+    {
+        Console.WriteLine("Введено некорректное число");
+    }
+}
+
+int number = int.Parse(Console.ReadLine()!);
+
+PrintCoordinatRange(number);
